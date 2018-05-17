dd10caa5cb20bca5f03651fbfcc1e5d4b567bdcc
cqbomb <605658506@qq.com>
2018-05-17 16:37:43 +0800

    readme

--- Charpter1_Python语言介绍/1.Scapy_Test.py
+++ Charpter1_Python语言介绍/1.Scapy_Test.py
@@ -2,5 +2,5 @@ from scapy.all import *
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)#清除报错
 
-ping_one_reply = sr1(IP(dst='196.21.5.254')/ICMP(), timeout = 1, verbose=False)
+ping_one_reply = sr1(IP(dst='172.16.1.254')/ICMP(), timeout = 1, verbose=False)
 ping_one_reply.show()
---
+++ Charpter1_Python语言介绍/README.md
@@ -0,0 +1,3 @@
+# Charpter1 Python语言介绍
+
+* 介绍Python语言特点
